c5411d52ebb261199d0373dde0f3c117ebc7a515
Jun Xu <xujun45@gmail.com>
2017-05-15 21:26:08 -0700

    fixed data type of database and enable auto commit

--- downloader.py
+++ downloader.py
@@ -1,29 +1,32 @@
 import pymysql
 from yahoo_finance import Share
 
-conn = pymysql.connect(host='localhost', user='yahoo', password='yahoopassword', db='yahoodb')
+conn = pymysql.connect(host='localhost', user='yahoo', password='yahoopassword', db='yahoodb', autocommit=True)
 cursor = conn.cursor()
 
-TABLENAME = 'record'
-company_list = ['YHOO']
+TABLENAME = 'yahooStock'
+company_list = ['YHOO', 'AAOI', 'KEM', 'EVRI', 'GOOGL', 'AMZN', 'FB', 'AAPL']
 START_DATE = '2017-05-07'
 END_DATE = '2017-05-10'
 
+#Create a new table to save data
 def createTable():
     dropTable = 'DROP TABLE IF EXISTS `' + TABLENAME + '`;'
     cursor.execute(dropTable)
-    sqlCreateTable = 'CREATE TABLE record (company_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,' \
-                     'Volume VARCHAR(30) NOT NULL, Symbol VARCHAR(30) NOT NULL, Adj_Close VARCHAR(30) NOT NULL, ' \
-                     'High VARCHAR(30) NOT NULL, Low VARCHAR(30) NOT NULL, DATE VARCHAR(30) NOT NULL, Close VARCHAR(30) NOT NULL,' \
-                     'Open VARCHAR(30) NOT NULL);'
+    sqlCreateTable = 'CREATE TABLE ' + TABLENAME +' (company_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,' \
+                     'Volume INT UNSIGNED NOT NULL, Symbol VARCHAR(30) NOT NULL, Adj_Close FLOAT UNSIGNED NOT NULL, ' \
+                     'High FLOAT UNSIGNED NOT NULL, Low FLOAT UNSIGNED NOT NULL, DATE DATE NOT NULL, Close FLOAT UNSIGNED NOT NULL,' \
+                     'Open FLOAT UNSIGNED NOT NULL);'
     cursor.execute(sqlCreateTable)
 
+#Select all data from table
 def selectAll():
     sqlSelect = 'SELECT * FROM ' + TABLENAME + ';'
     numRows = cursor.execute(sqlSelect)
     for row in range(numRows):
         print cursor.fetchone()
 
+#Insert data into table
 def insertRow(l):
     sqlInsert = 'INSERT INTO ' + TABLENAME + ' VALUE (NULL'
     for key in l.keys():
@@ -33,6 +36,7 @@ def insertRow(l):
 
 createTable()
 selectAll()
+#loop through all companies
 for company in company_list:
     comp = Share(company)
     allHistory = comp.get_historical(START_DATE, END_DATE)
@@ -44,22 +48,3 @@ selectAll()
 cursor.close()
 conn.close()
 
-
-# sqlInsert = 'INSERT INTO stock VALUE'
-#
-#
-# for n in l:
-#     print n['Volume']
-#
-# conn = pymysql.connect(host='localhost', user='yahoo', password='yahoopassword', db='yahoodb')
-#
-# a = conn.cursor()
-
-# sql = 'SELECT * from `stock`;'
-# a.execute(sql)
-#
-# countrow = a.execute(sql)
-# print ("Number of row :", countrow)
-#
-# data = a.fetchall()
-# print(data)
